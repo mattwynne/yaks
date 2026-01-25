72e4212872e9df1eb00b3abab4b9d088d5c40a8f
Matt Wynne <matt@mattwynne.net>
2026-01-25 00:02:40 -0800

    Sort list output by done status then creation time
    - Done yaks appear first
    - Within each group, sort by creation time (oldest first)
    - Add test to verify sorting behavior

--- spec/list.sh
+++ spec/list.sh
@@ -21,4 +21,17 @@ Describe 'yx list'
     When run sh -c 'yx add "Fix the bug" && yx ls'
     The output should equal "- [ ] Fix the bug"
   End
+
+  It 'sorts yaks with done first, then by creation time (oldest first)'
+    When run sh -c '
+      yx add "oldest" && sleep 0.1 &&
+      yx add "middle" && sleep 0.1 &&
+      yx add "newest" &&
+      yx done "middle" &&
+      yx list
+    '
+    The line 1 should equal "- [x] middle"
+    The line 2 should equal "- [ ] oldest"
+    The line 3 should equal "- [ ] newest"
+  End
 End
