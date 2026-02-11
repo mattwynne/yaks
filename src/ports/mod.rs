84401488737408b8f2b3e95b9a222cbc544a06b1
Matt Wynne <matt.wynne@mechanical-orchard.com>
2026-02-10 23:46:07 -0800

    Add EventListener trait for event projections
    EventListener trait allows infrastructure to react
    to domain events. Projections implement this to
    update their state.

--- src/ports/mod.rs
+++ src/ports/mod.rs
@@ -1,12 +1,14 @@
 // Port traits - define interfaces between domain and adapters
 
 pub mod display;
+pub mod event_listener;
 pub mod input;
 pub mod log;
 pub mod storage;
 pub mod sync;
 
 pub use display::DisplayPort;
+pub use event_listener::EventListener;
 pub use input::InputPort;
 pub use log::LogPort;
 pub use storage::StoragePort;
