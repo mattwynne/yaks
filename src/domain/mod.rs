4ac0da542cd4d93f488e330d0852eb9741312979
Matt Wynne <matt.wynne@mechanical-orchard.com>
2026-02-10 23:42:46 -0800

    Define YakEvent enum with domain events
    Replace Event struct with YakEvent enum
    containing:
    - Added, Removed, Moved (lifecycle)
    - ContextUpdated, StateUpdated, FieldUpdated
      (attributes)
    
    Events contain only domain data, no timestamps.
    
    Keep legacy Event struct temporarily for backward
    compatibility during refactoring.

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -6,6 +6,6 @@ pub mod field;
 pub mod yak;
 
 #[allow(unused_imports)]
-pub use event::Event;
+pub use event::{Event, YakEvent};
 pub use field::{validate_field_name, CONTEXT_FIELD, STATE_FIELD};
 pub use yak::{validate_yak_name, Yak};
