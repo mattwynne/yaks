8540e0105ff121d0beac1d323e66c29226e68f32
Matt Wynne <matt.wynne@mechanical-orchard.com>
2026-02-11 00:19:44 -0800

    Add InMemoryEventStore adapter
    In-memory implementation of EventStore for testing
    and initial development.

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,6 +1,7 @@
 // Adapters - implementations of port traits for specific technologies
 
 pub mod cli;
+pub mod event_store;
 pub mod input;
 pub mod log;
 pub mod storage;
@@ -9,6 +10,8 @@ pub mod sync;
 // Re-export commonly used adapters for external use
 #[allow(unused_imports)]
 pub use cli::{ConsoleDisplay, InMemoryDisplay};
+#[allow(unused_imports)]
+pub use event_store::InMemoryEventStore;
 #[allow(unused_imports)]
 pub use input::{ConsoleInput, InMemoryInput};
 #[allow(unused_imports)]
