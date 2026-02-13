0997b6f01bc0fc75d97e29004e7ca874b82fb6cf
Matt Wynne <matt.wynne@mechanical-orchard.com>
2026-02-12 19:57:41 -0800

    Add acceptance tests for yx completions
    Create thin acceptance tests that verify the
    completions subcommand works end-to-end. These
    complement the existing Rust unit tests by
    proving the binary wiring is correct.

---
+++ spec/features/completion_acceptance.sh
@@ -0,0 +1,29 @@
+# shellcheck shell=bash
+# shellcheck disable=SC1010
+Describe 'yx completions (acceptance)'
+  BeforeEach 'setup_isolated_repo'
+  AfterEach 'teardown_isolated_repo'
+
+  It 'lists commands'
+    When run yx completions -- yx ""
+    The status should be success
+    The output should include "add"
+    The output should include "done"
+    The output should include "remove"
+  End
+
+  It 'lists yak names for rm'
+    yx add "my-yak"
+    When run yx completions -- yx rm ""
+    The output should include "my-yak"
+  End
+
+  It 'filters done yaks for done command'
+    yx add "todo-yak"
+    yx add "done-yak"
+    yx done "done-yak"
+    When run yx completions -- yx done ""
+    The output should include "todo-yak"
+    The output should not include "done-yak"
+  End
+End
